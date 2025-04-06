8f3a9c8f38dbdba4b8c5716a25268007837394a9
katsuki <katsuki.suzu03@gmail.com>
2025-04-07 01:29:17 +0900

    parse.cをtokenizeとprogramに分離

---
+++ tokenize.c
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "9cc.h"
+
+
+int is_alnumunder(char c) {
+  return ('a' <= c && c <= 'z') ||
+         ('A' <= c && c <= 'Z') ||
+         ('0' <= c && c <= '9') ||
+         (c == '_');
+}
+
+// 新しいトークンを作成してcurに繋げる
+Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  tok->len = len;
+  cur->next = tok;
+  return tok;
+}
+
+// 入力文字列pをトークナイズしてそれを返す
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    // 空白文字をスキップ
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    // multi letter (len = 2: fixed)
+    if (startswith(p, "==") || startswith(p, "!=") || startswith(p, "<=") || startswith(p, ">=")) {
+      cur = new_token(TK_RESERVED, cur, p, 2);
+      p += 2;
+      continue;
+    }
+
+    // single letter
+    if (strchr("+-*/()<>;=", *p)) {
+      cur = new_token(TK_RESERVED, cur, p++, 1);
+      continue;
+    }
+
+    // integer
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p, 0);
+      char *q = p;
+      cur->val = strtol(p, &p, 10);
+      cur->len = p - q;
+      continue;
+    }
+
+    // return
+    if (strncmp(p, "return", 6) == 0 && !is_alnumunder(p[6])) {
+      cur = new_token(TK_RETURN, cur, p, 6);
+      p += 6;
+      continue;
+    }
+
+    // 英小文字or英大文字or数字orアンダーバーで先頭数字はだめ(isdigitで解釈されるはず)
+    // 暫定的に英小文字だけとする
+    if (is_alnumunder(*p)) {
+      char *q = p;
+      while (is_alnumunder(*p))
+        p++;
+      cur = new_token(TK_IDENT, cur, q, p - q);
+      continue;
+    }
+
+    error_at(p, "トークナイズできません");
+  }
+
+  new_token(TK_EOF, cur, p, 0);
+  return head.next;
+}
