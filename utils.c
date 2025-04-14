3a1963d18682621fac4ad87ca59dab18de3d704e
katsuki <katsuki.suzu03@gmail.com>
2025-04-14 20:35:25 +0900

    error.cをutils.cへ移植

--- utils.c
+++ utils.c
@@ -1,6 +1,31 @@
-#include "9cc.h"
 #include <stdbool.h>
 #include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "9cc.h"
+
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " "); /* pos個の空白を出力 */
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
 
 bool startswith(char *p, char *q) {
   return memcmp(p, q, strlen(q)) == 0;
